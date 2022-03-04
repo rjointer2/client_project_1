e02d7807cce31147ac1bee61f61c1efe50b81a7d
Roody <rjointer2@gmail.com>
2022-03-04 14:02:14 -0800

    hello world

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Next App</title>
+        
+      </Head>
+
+      <main>
+        Hello World
+      </main>
+
+      <footer>
+        I'm a footer
+      </footer>
+    </div>
+  )
+}
+
+export default Home
