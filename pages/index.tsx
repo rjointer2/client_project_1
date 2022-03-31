d7bb5f3fb6c7f09ef88ecf23fac8afaf92330d2c
Roody <rjointer2@gmail.com>
2022-03-31 11:24:50 -0700

    the server sends the derivations of the x and y positions and now we can show the vectors changes the ui, so now we can start working on ther server's collision physics

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,17 +10,42 @@ import { Rectangle, ServerClientDiction } from '../typeDef/gameTypeDefs';
 const socket = io('http://localhost:1212');
 
 const Home: NextPage = () => {
+
+  class Vector {
+    x: number;
+    y: number;
+    constructor( x: number , y: number ) {
+      this.x =  x
+      this.y =  y
+    }
+
+    add( v: Vector ) {
+      return new Vector( this.x+v.x, this.y+v.y )
+    }
+
+    subtract ( v: Vector ) {
+      return new Vector( this.x-v.x, this.y-v.y )
+    }
+
+    mag() {
+      return Math.sqrt( this.x**2 + this.y**2 )
+    }
+
+    mult( n: number ) {
+      return new Vector( this.x*n, this.y*n )
+    } 
+ 
+
+  }
  
   const renderCount = useRef(0);
-
-  const [ pos, setPos ] = useState({ x: 0, y: 0 })
 
   useEffect(() => {
     renderCount.current = renderCount.current + 1
     console.log(renderCount.current)
   })
 
-  const [ clis, setClis ] = useState({ a: 0, b: 0 })
+  const [ clis, setClis ] = useState({})
   const [ id, setId ] = useState('')
 
   console.log(id)
@@ -49,20 +74,36 @@ const Home: NextPage = () => {
     if(!renderContext) return;
     setCtx(renderContext)
 
-    socket.emit('newClient', { color: 'red', x: Math.floor(Math.random() * 200), y: 200 })
+    socket.emit('newClient', { color: 'red', x: 320, y: 240, host: false, dx: 0, dy: 0 })
     socket.on('registerId', clientId => setId(clientId))
     socket.on('position', data => {
       renderContext.clearRect( 0, 0, 640, 480 )
+
       for( let i in data ) {
+
         renderContext.fillRect(
-          data[i].x, data[i].y, 20, 20
+          data[i].x - 10, data[i].y - 10, 20, 20
         )
+
+        renderContext.beginPath();
+        renderContext.moveTo(data[i].x, data[i].y)
+        renderContext.lineTo( data[i].x - data[i].dx * 5, data[i].y )
+        renderContext.strokeStyle = "red";
+        renderContext.stroke();
+
+        renderContext.beginPath();
+        renderContext.moveTo(data[i].x, data[i].y)
+        renderContext.lineTo( data[i].x, data[i].y - data[i].dy * 5 )
+        renderContext.strokeStyle = "blue";
+        renderContext.stroke();
+
       }
       setClis( data )
-      console.log(data)
+      console.log(data[id])
     })
 
   }, [ctx])
+
 
   return <div>
 
@@ -72,10 +113,10 @@ const Home: NextPage = () => {
       style={{ border: "1px solid black" }}
     ></canvas>
     <br/>
-    position( x: { pos.x }, y: { pos.y } )
     
   </div>
 
 }
 
 export default Home
+
